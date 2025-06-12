258d90ad6cbe3f31609f851af6bef8ebc318c760
xiong <jhang.xiong@gmail.com>
2025-06-12 17:44:18 +0800

    update trackastra
    - add support for dir input

--- rnaflow/cell/pipeline/track/trackastra_api.py
+++ rnaflow/cell/pipeline/track/trackastra_api.py
@@ -1,17 +1,25 @@
 from pathlib import Path
+from typing import Any, Optional, Union
 
 import numpy as np
+import tifffile as tiff
 
 import torch
 from trackastra.model import Trackastra
 from trackastra.tracking import graph_to_ctc, graph_to_napari_tracks
 from trackastra.data import example_data_bacteria
 
-def cell_track(imgs: np.ndarray, masks: np.ndarray, device: str,
+def cell_track(imgs: Union[Path, np.ndarray], masks: Union[Path, np.ndarray], device: str,
                out_dir: Path | None = None, 
                model_name: str = "general_2d", mode: str = "greedy"
             ):
-    
+    """Track cells in a sequence of images using the Trackastra model."""
+    if isinstance(imgs, Path):
+        paths = sorted(imgs.glob('*.tif'))
+        imgs = np.stack([tiff.imread(p) for p in paths], axis=0)
+        paths = sorted(masks.glob('*.tif'))
+        masks = np.stack([tiff.imread(p) for p in paths], axis=0)
+
     assert imgs.ndim == 3 and imgs.shape[0] > 1, "Input images should be a 3D array (time, height, width)"
     assert imgs.shape == masks.shape, "Input images and masks should have the same shape"
 
