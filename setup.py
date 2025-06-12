9a9d307b0010443f95361c6933d9b6ae26834bbf
xiong <jhang.xiong@gmail.com>
2025-06-12 12:42:56 +0800

    init rnaflow package and setting

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rnaflow",          
+    version="0.1",               
+    packages=find_packages(),    
+    
+    author="Xiaofei Xiong",
+    author_email="jhang.xiong@gmail.com",
+    description="Code for RNA Transcription Kinetics Analysis",
+    license="MIT",
+    url="https://github.com/xiongjhang/rnaflow",
+)
