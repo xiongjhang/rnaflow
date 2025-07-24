2662daf9102c20e3d987e5f04b4c6acdc5433f94
xiong <jhang.xiong@gmail.com>
2025-07-24 16:16:14 +0800

    [cell-track]: Init match.py

---
+++ rnaflow/cell/match.py
@@ -0,0 +1,4 @@
+""" Matcher Code Reference.
+    - https://github.com/facebookresearch/detr/blob/29901c51d7fe8712168b8d0d64351170bc0f83e0/models/matcher.py#L12
+    - https://github.com/SIPEC-Animal-Data-Analysis/SIPEC/blob/d1938512002b21643ec28d16c9ad5e7720a13bbf/SwissKnife/masksmoothing.py#L16
+"""
